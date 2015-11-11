4b5c90fb11ce1ad6648ce53edf36c1182897aa06
Jhon <jfh_flores_11@hotmail.com>
2015-11-10 19:00:48 -0500

    monto de caja

--- src/main/java/org/sistemafinanciero/controller/ReporteCajaBancosServiceBeanNT.java
+++ src/main/java/org/sistemafinanciero/controller/ReporteCajaBancosServiceBeanNT.java
@@ -56,7 +56,7 @@ public class ReporteCajaBancosServiceBeanNT implements ReporteCajaBancosServiceN
 		
 		BigDecimal montoBovedasCajas = montoBovedas.add(montoCajas);
 		
-		
+		System.out.println("monto caja: " + montoCajas + "// monto bovedas: " + montoBovedas);
 		
 		return montoBovedasCajas;
 	}
